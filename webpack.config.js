7730b08a64c7de9b41deb36cec8b85c45f398226
shefy1 <1912510604@qq.com>
2020-05-09 01:01:00 +0800

    解析图片

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,11 @@ module.exports = {
                 loader: 'babel-loader', // 使用babel-loader编译
                 exclude: /node_modules/ // node_module里面的内容不遍历
             },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] }
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }, // 解析样式
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] }, //解析样式
+            { test: /\.(jpg|png|gif)$/, use: 'url-loader?limit=8192' }, // 解析图片
+            // limit表示转化base64只在8192字节以下转化，其他情况输出图片 
+            { test:/\.(eot|svg|woff|woff2|wtf)$/,use:'url-loader'} // 解析图片
         ]
     }
 }
