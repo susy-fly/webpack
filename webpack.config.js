f2a296ec6b23467670f9961bbb4be434d055cdd0
shefy1 <1912510604@qq.com>
2020-05-09 00:54:00 +0800

    babel编译

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'), // 出口路径和目录
         filename: 'demo.js' // 编译后的名称
+    },
+    module: {
+        rules: [ // 遍历规则
+            {
+                test: /\.js$/, // 匹配以.js结尾的文件
+                loader: 'babel-loader', // 使用babel-loader变异
+                exclude: /node_modules/ // node_module里面的内容不遍历
+            }
+        ]
     }
 }
